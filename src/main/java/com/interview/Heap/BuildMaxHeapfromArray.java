c0d1607dc15f528314998b523bf3e479fb22fb1c
Subrata Kundu <subrata.kundu30@gmail.com>
2019-01-15 17:30:46 +0530

    Minor changes

--- src/main/java/com/interview/Heap/BuildMaxHeapfromArray.java
+++ src/main/java/com/interview/Heap/BuildMaxHeapfromArray.java
@@ -13,12 +13,12 @@ public class BuildMaxHeapfromArray {
 		//int A[] = {1, 14, 10, 8, 7, 9, 3, 2, 4, 6};
 		int A[] = {12, 20, 2, 13, 32, 50, 88, 21, 18, 91};
 		System.out.println("Given input array is: "+ Arrays.toString(A));
-		buildMaxHeap(A);
+		buildMaxHeap(A, A.length);
 		System.out.println("After Max Heapify array is: "+ Arrays.toString(A));
 	}
 	
-	protected static void buildMaxHeap(int[] A){
-		for(int i = (A.length -1)/2 ; i>=0 ;i--){
+	protected static void buildMaxHeap(int[] A, int size){
+		for(int i = (size -1)/2 ; i>=0 ;i--){
 			maxHeapify(A, i);
 		}
 	}
