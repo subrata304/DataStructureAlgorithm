41ab9abeb180d06f03ef747a5f36d8c6a7d5e595
Subrata Kundu <subrata.kundu30@gmail.com>
2019-01-16 03:09:53 +0530

    Find kth largest elements from an array

---
+++ src/main/java/com/interview/Heap/FindKthLargestElementFromArray.java
@@ -0,0 +1,34 @@
+package com.interview.Heap;
+
+/*
+ * Find and print the largest 3 elements from an unsorted array
+ * 
+ */
+
+public class FindKthLargestElementFromArray {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {1, 23, 12, 9, 30, 2, 50};
+		int k = 3;
+	
+		/*
+		 * Steps:
+		 * 1. Build a Max heap --> O(n) times
+		 * 2. Use DeleteMaxElement k times to get k largest elements --> O(klogn)
+		 * 
+		 * Total Time Complexity: O(n + klogn)
+		 * 
+		 */
+		
+		BuildMaxHeapfromArray.buildMaxHeap(arr, arr.length);
+		int size = arr.length;
+		for(int i = 0; i < k ; i++) {
+			int maxElement = DeleteMaxElementfromMaxHeap.deleteMaxElement(arr);
+			size--;
+			BuildMaxHeapfromArray.maxHeapify(arr, size, 0);
+			System.out.println("The Max elements are: " + maxElement);
+		}
+		
+	}
+}
