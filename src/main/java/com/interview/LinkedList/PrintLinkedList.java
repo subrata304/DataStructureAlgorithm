866ab2eac3db2820c6fb7e25a52346d1e190e34a
Subrata Kundu <subrata.kundu30@gmail.com>
2019-02-18 01:23:15 +0530

    Adding new way to print LL and minor changes

--- src/main/java/com/interview/LinkedList/PrintLinkedList.java
+++ src/main/java/com/interview/LinkedList/PrintLinkedList.java
@@ -13,4 +13,16 @@ public class PrintLinkedList {
 		}
 		System.out.println();
 	}
+	
+	public static void printList(Node node) {
+		Node temp = node;
+		while(temp != null) {
+			System.out.print(temp.getData());
+			if(temp.getNext()!= null) {
+				System.out.print(" -> ");
+			}
+			temp = temp.getNext();
+		}
+		System.out.println();
+	}
 }
